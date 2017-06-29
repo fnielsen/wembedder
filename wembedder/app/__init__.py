ca3866911acd48ec1d43cb865d7ad4c20f26016a
fnielsen <fn@imm.dtu.dk>
2017-06-29 20:40:54 +0200

    Create __init__.py

---
+++ wembedder/app/__init__.py
@@ -0,0 +1,33 @@
+"""app."""
+
+
+from __future__ import absolute_import, division, print_function
+
+from flask import Flask
+from flask_bootstrap import Bootstrap, StaticCDN
+
+from ..model import Model, MODEL_FILENAME
+
+
+def create_app():
+    """Create webapp.
+
+    Factory for webapp.
+
+    Returns
+    -------
+    app : flask.app.Flask
+        Flask app object.
+
+    """
+    app = Flask(__name__)
+    app.model = Model.load()
+
+    Bootstrap(app)
+
+    app.extensions['bootstrap']['cdns']['bootstrap'] = StaticCDN()
+    
+    from .views import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
