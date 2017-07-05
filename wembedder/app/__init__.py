9ac9246b1e751fb0119cccbcf9fe4a493a02a5aa
fnielsen <fn@imm.dtu.dk>
2017-07-05 15:46:14 +0200

    Fix after change in model loading

--- wembedder/app/__init__.py
+++ wembedder/app/__init__.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import, division, print_function
 from flask import Flask
 from flask_bootstrap import Bootstrap, StaticCDN
 
-from ..model import Model, MODEL_FILENAME
+from ..model import Model
 
 
 def create_app():
@@ -26,7 +26,7 @@ def create_app():
     Bootstrap(app)
 
     app.extensions['bootstrap']['cdns']['bootstrap'] = StaticCDN()
-    
+
     from .views import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
