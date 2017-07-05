befe9eac4c52456e5e75a4d17a365e16dcf4d7b9
fnielsen <fn@imm.dtu.dk>
2017-07-05 15:37:28 +0200

    Change model loading

--- wembedder/model.py
+++ wembedder/model.py
@@ -1,22 +1,42 @@
 """Interface to Gensim models."""
 
 
+from os import listdir
+from os.path import expanduser, isdir, join, split
+
 from gensim.models import Word2Vec
 
 
-# TODO: Make canonical data directory
-MODEL_FILENAME = (
-    '/home/faan/data/wikidata/'
-    'wikidata-20170613-truthy-BETA-size=100-window=1-min_count=20'
-)
+MODELS_DIRECTORY = join(expanduser('~'), 'wembedder_data', 'models')
 
 
 class Model(Word2Vec):
 
     @classmethod
-    def load(cls, filename=MODEL_FILENAME):
-        model = super(Model, cls).load(filename)
-        model.metadata = {
-            'filename': filename
-        }
+    def load(cls, models_directory=MODELS_DIRECTORY):
+        subdirectories = [
+            join(models_directory, subdirectory)
+            for subdirectory in listdir(models_directory)
+            if isdir(join(models_directory, subdirectory))
+        ]
+        if len(subdirectories) == 0:
+            model = Model()
+            model.metadata = {
+                'error': 'No models available'
+            }
+            return model
+
+        subdirectory = subdirectories[0]
+        filename = split(subdirectory)[-1]
+        
+        try: 
+            model = super(Model, cls).load(join(subdirectory, filename))
+            model.metadata = {
+                'filename': filename
+            }
+        except FileNotFoundError:
+            model = Model()
+            model.metadata = {
+                'error': 'File not found: {}'.format(filename)
+            }
         return model
