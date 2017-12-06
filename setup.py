e5ed1b89ccd2924237d99a6f8fea7d7b3db1e44a
fnielsen <fn@imm.dtu.dk>
2017-12-06 15:49:08 +0100

    Clean up of setup parameters
    Version is now set from versioneer.
    Some Python versions has not been tested.
    They might work

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ requirements = open(filename).read().splitlines()
 setup(
     name='wembedder',
     packages=['wembedder'],
-    version='0.1',
     author='Finn Aarup Nielsen',
     author_email='faan@dtu.dk',
     description='Wikidata embedding',
@@ -24,12 +23,9 @@ setup(
     package_data={'wembedder': ['data/*.csv']},
     long_description='',
     classifiers=[
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        ],
+    ],
     cmdclass==versioneer.get_cmdclass(),
     install_requires=requirements,
     version=versioneer.get_version(),
