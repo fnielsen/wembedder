dd384db9a021cb7e3682f4153226d5fe53d05c9f
fnielsen <fn@imm.dtu.dk>
2018-08-07 11:37:52 +0200

    Implement command-line handling

---
+++ wembedder/__main__.py
@@ -0,0 +1,27 @@
+"""Handle command-line input.
+
+Usage:
+  wembedder most-similar <q>
+
+"""
+
+
+from __future__ import print_function
+
+from model import Model
+
+from docopt import docopt
+
+
+arguments = docopt(__doc__)
+q = arguments['<q>']
+
+# Load model
+model = Model.load()
+
+# Compute similarities
+results = model.most_similar(q)
+
+# Report results
+for result in results:
+    print(result[0])
